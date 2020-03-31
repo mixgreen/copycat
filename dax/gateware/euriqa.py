61e3bab5b4f1a2a4fcf050442b104de8a814280b
Leon Riesebos <leon.riesebos@duke.edu>
2020-03-31 14:11:17 -0400

    re-inserted red chamber gateware into repository and merged build script with kc705 target file. code might need to be verified again.

---
+++ dax/gateware/euriqa.py
@@ -0,0 +1,183 @@
+"""Extension card for the EURIQA project.
+
+Describes the "FMC" adapter that plugs into the KC705 and interfaces with the
+Sandia/Duke Pulser hardware: DDS's, ADC, DAC, GPIO (TTL), etc.
+
+Description of the pins & interfaces available on the FMC connector.
+"""
+from migen.build.generic_platform import IOStandard
+from migen.build.generic_platform import Pins
+from migen.build.generic_platform import Subsignal
+
+fmc_adapter_io = [
+    # Input Bank 1
+    ("in1", 0, Pins("HPC:HA13_N"), IOStandard("LVCMOS33")),
+    ("in1", 1, Pins("HPC:HA02_N"), IOStandard("LVCMOS33")),
+    ("in1", 2, Pins("HPC:HA14_P"), IOStandard("LVCMOS33")),
+    ("in1", 3, Pins("HPC:HA02_P"), IOStandard("LVCMOS33")),
+    ("in1", 4, Pins("HPC:HA14_N"), IOStandard("LVCMOS33")),
+    ("in1", 5, Pins("HPC:HA01_CC_N"), IOStandard("LVCMOS33")),
+    ("in1", 6, Pins("HPC:LA23_P"), IOStandard("LVCMOS33")),
+    ("in1", 7, Pins("HPC:LA28_N"), IOStandard("LVCMOS33")),
+    # Input Bank 2
+    ("in2", 0, Pins("HPC:HA11_N"), IOStandard("LVCMOS33")),
+    ("in2", 1, Pins("HPC:HA19_P"), IOStandard("LVCMOS33")),
+    ("in2", 2, Pins("HPC:HA12_P"), IOStandard("LVCMOS33")),
+    ("in2", 3, Pins("HPC:HA04_P"), IOStandard("LVCMOS33")),
+    ("in2", 4, Pins("HPC:HA12_N"), IOStandard("LVCMOS33")),
+    ("in2", 5, Pins("HPC:HA03_N"), IOStandard("LVCMOS33")),
+    ("in2", 6, Pins("HPC:HA13_P"), IOStandard("LVCMOS33")),
+    ("in2", 7, Pins("HPC:HA03_P"), IOStandard("LVCMOS33")),
+    # Input Bank 3
+    ("in3", 0, Pins("HPC:HA04_N"), IOStandard("LVCMOS33")),
+    ("in3", 1, Pins("HPC:HA05_P"), IOStandard("LVCMOS33")),
+    ("in3", 2, Pins("HPC:HA05_N"), IOStandard("LVCMOS33")),
+    ("in3", 3, Pins("HPC:HA06_P"), IOStandard("LVCMOS33")),
+    ("in3", 5, Pins("HPC:HA06_N"), IOStandard("LVCMOS33")),
+    ("in3", 4, Pins("HPC:HA07_P"), IOStandard("LVCMOS33")),
+    ("in3", 6, Pins("HPC:HA07_N"), IOStandard("LVCMOS33")),
+    ("in3", 7, Pins("HPC:HA08_P"), IOStandard("LVCMOS33")),
+    # Output Bank 1
+    ("out1", 0, Pins("HPC:LA03_N"), IOStandard("LVCMOS33")),
+    ("out1", 1, Pins("HPC:LA03_P"), IOStandard("LVCMOS33")),
+    ("out1", 2, Pins("HPC:LA02_N"), IOStandard("LVCMOS33")),
+    ("out1", 3, Pins("HPC:LA02_P"), IOStandard("LVCMOS33")),
+    ("out1", 4, Pins("HPC:LA01_CC_N"), IOStandard("LVCMOS33")),
+    ("out1", 5, Pins("HPC:LA01_CC_P"), IOStandard("LVCMOS33")),
+    ("out1", 6, Pins("HPC:LA00_CC_N"), IOStandard("LVCMOS33")),
+    ("out1", 7, Pins("HPC:LA00_CC_P"), IOStandard("LVCMOS33")),
+    # Output Bank 2
+    ("out2", 0, Pins("HPC:LA07_N"), IOStandard("LVCMOS33")),
+    ("out2", 1, Pins("HPC:LA07_P"), IOStandard("LVCMOS33")),
+    ("out2", 3, Pins("HPC:LA06_N"), IOStandard("LVCMOS33")),
+    ("out2", 2, Pins("HPC:LA06_P"), IOStandard("LVCMOS33")),
+    ("out2", 4, Pins("HPC:LA05_N"), IOStandard("LVCMOS33")),
+    ("out2", 5, Pins("HPC:LA05_P"), IOStandard("LVCMOS33")),
+    ("out2", 6, Pins("HPC:LA04_N"), IOStandard("LVCMOS33")),
+    ("out2", 7, Pins("HPC:LA04_P"), IOStandard("LVCMOS33")),
+    # This is out8 on the board, but out3 on the front panel
+    ("out3", 0, Pins("HPC:LA32_N"), IOStandard("LVCMOS33")),
+    ("out3", 1, Pins("HPC:LA09_N"), IOStandard("LVCMOS33")),
+    ("out3", 2, Pins("HPC:LA32_P"), IOStandard("LVCMOS33")),
+    ("out3", 3, Pins("HPC:LA09_P"), IOStandard("LVCMOS33")),
+    ("out3", 4, Pins("HPC:LA28_P"), IOStandard("LVCMOS33")),
+    ("out3", 5, Pins("HPC:LA08_N"), IOStandard("LVCMOS33")),
+    ("out3", 6, Pins("HPC:LA30_P"), IOStandard("LVCMOS33")),
+    ("out3", 7, Pins("HPC:HA08_P"), IOStandard("LVCMOS33")),
+
+    # This is out9 on the board, but out4 on the front panel
+    ("out4", 0, Pins("HPC:LA19_P"), IOStandard("LVCMOS33")),
+    ("out4", 1, Pins("HPC:LA11_N"), IOStandard("LVCMOS33")),
+    ("out4", 2, Pins("HPC:LA19_N"), IOStandard("LVCMOS33")),
+    ("out4", 3, Pins("HPC:LA11_P"), IOStandard("LVCMOS33")),
+    ("out4", 4, Pins("HPC:LA33_N"), IOStandard("LVCMOS33")),
+    ("out4", 5, Pins("HPC:LA10_N"), IOStandard("LVCMOS33")),
+    ("out4", 6, Pins("HPC:LA33_P"), IOStandard("LVCMOS33")),
+    ("out4", 7, Pins("HPC:LA10_P"), IOStandard("LVCMOS33")),
+    # SMA output/input??
+    ("sma", 0, Pins("HPC:HA09_P"), IOStandard("LVCMOS33")),
+    ("sma", 1, Pins("HPC:LA24_P"), IOStandard("LVCMOS33")),
+    ("sma", 2, Pins("HPC:LA24_N"), IOStandard("LVCMOS33")),
+    ("sma", 3, Pins("HPC:HA19_N"), IOStandard("LVCMOS33")),
+    ("sma", 4, Pins("HPC:HA10_N"), IOStandard("LVCMOS33")),
+    ("sma", 5, Pins("HPC:LA20_N"), IOStandard("LVCMOS33")),
+    ("sma", 6, Pins("HPC:HA11_P"), IOStandard("LVCMOS33")),
+    ("sma", 7, Pins("HPC:HA09_N"), IOStandard("LVCMOS33")),
+    ("sma", 8, Pins("HPC:LA20_P"), IOStandard("LVCMOS33")),
+    # OEB = Output enable buffer
+    ("oeb", 0, Pins("HPC: HA08_N"), IOStandard("LVCMOS33")),
+    # trigger for updating output of the DDS
+    ("io_update", 0, Pins("LPC:LA22_P"), IOStandard("LVCMOS33")),  # dac8734 ldac (MEMS)
+    ("io_update", 1, Pins("LPC:LA22_N"), IOStandard("LVCMOS33")),  # dac8734 out (MEMS)
+    ("io_update", 2, Pins("HPC:HA00_CC_N"), IOStandard("LVCMOS33")),
+    ("io_update", 3, Pins("HPC:HA18_P"), IOStandard("LVCMOS33")),
+    ("io_update", 4, Pins("HPC:LA22_P"), IOStandard("LVCMOS33")),
+    ("io_update", 5, Pins("HPC:LA26_P"), IOStandard("LVCMOS33")),
+    ("io_update", 6, Pins("HPC:LA16_P"), IOStandard("LVCMOS33")),
+    ("io_update", 7, Pins("HPC:LA14_N"), IOStandard("LVCMOS33")),
+    ("io_update", 8, Pins("HPC:LA18_CC_P"), IOStandard("LVCMOS33")),
+    ("io_update", 9, Pins("HPC:LA12_N"), IOStandard("LVCMOS33")),
+    # Resets for the DDS control boards
+    ("reset", 0, Pins("LPC:LA23_P"), IOStandard("LVCMOS33")),  # dac8734 clk (MEMS)
+    ("reset", 1, Pins("HPC:HA01_CC_P"), IOStandard("LVCMOS33")),
+    ("reset", 2, Pins("HPC:LA22_N"), IOStandard("LVCMOS33")),
+    ("reset", 3, Pins("HPC:LA16_N"), IOStandard("LVCMOS33")),
+    ("reset", 4, Pins("HPC:LA18_CC_N"), IOStandard("LVCMOS33")),
+    # data channel for odd DDS channels on the DDS board,
+    # Because the data channels are separated
+    # the even channels are in the mosi Subsignal of the SPI busses
+    ("odd_channel_sdio", 0, Pins("LPC:LA23_N"), IOStandard("LVCMOS33")),  # dac8734 cs (MEMS)
+    ("odd_channel_sdio", 1, Pins("HPC:HA18_N"), IOStandard("LVCMOS33")),
+    ("odd_channel_sdio", 2, Pins("HPC:LA26_N"), IOStandard("LVCMOS33")),
+    ("odd_channel_sdio", 3, Pins("HPC:LA14_P"), IOStandard("LVCMOS33")),
+    ("odd_channel_sdio", 4, Pins("HPC:LA12_P"), IOStandard("LVCMOS33")),
+    # SPI outputs
+    (
+        "spi",  # used for MEMS system
+        0,
+        Subsignal("clk", Pins("LPC:LA20_P")),  # hv209 clk (MEMS)
+        Subsignal("cs_n", Pins("LPC:LA20_N LPC:LA21_P")),  # hv209 clr (MEMS), hv209 d_in (MEMS)
+        Subsignal("mosi", Pins("LPC:LA21_N")),  # dac8734 reset (MEMS)
+        IOStandard("LVCMOS33"),
+    ),
+    # SPI channels for communicating to DDS boards
+    (
+        "spi",
+        1,
+        Subsignal("clk", Pins("HPC:HA16_N")),
+        Subsignal("cs_n", Pins("HPC:HA17_CC_P HPC:HA00_CC_P")),
+        Subsignal("mosi", Pins("HPC:HA17_CC_N")),
+        IOStandard("LVCMOS33"),
+    ),
+    (
+        "spi",
+        2,
+        Subsignal("clk", Pins("HPC:LA21_P")),
+        Subsignal("cs_n", Pins("HPC:LA25_P HPC:LA21_N")),
+        Subsignal("mosi", Pins("HPC:LA25_N")),
+        IOStandard("LVCMOS33"),
+    ),
+    (
+        "spi",
+        3,
+        Subsignal("clk", Pins("HPC:LA27_P")),
+        Subsignal("cs_n", Pins("HPC:LA23_N HPC:LA27_N")),
+        Subsignal("mosi", Pins("HPC:LA15_P")),
+        IOStandard("LVCMOS33"),
+    ),
+    (
+        "spi",
+        4,
+        Subsignal("clk", Pins("HPC:LA17_CC_P")),
+        Subsignal("cs_n", Pins("HPC:LA13_N HPC:LA17_CC_N")),
+        Subsignal("mosi", Pins("HPC:LA13_P")),
+        IOStandard("LVCMOS33"),
+    ),
+    # DAC8568 Control pins: SPI & Load DAC TTL/GPIO trigger (LDAC)
+    (
+        "spi",
+        5,
+        Subsignal("clk", Pins("HPC: HA10_P")),
+        Subsignal("cs_n", Pins("HPC: HA23_P")),
+        Subsignal("mosi", Pins("HPC: HA23_N")),
+        IOStandard("LVCMOS33"),
+    ),
+    ("ldac", 0, Pins("HPC: HA21_P"), IOStandard("LVCMOS33")),
+]
+
+x100_dac_spi = [
+    # SPI to hack UART-like communication to Sandia DAC. Takes over out2-7.
+    # SHOULD NOT ALWAYS BE USED. Only if using real-time comm to 100x DAC.
+    # Only relevant pin is "miso", overwrites the SD card pins.
+    # COULD SCREW UP YOUR SD CARD. CAREFUL
+    (
+        "spi",
+        6,
+        Subsignal("clk", Pins("AB22")),  # Unassigned # AB22 = SD Card MOSI
+        Subsignal("mosi", Pins("HPC:LA04_P")),
+        Subsignal("cs_n", Pins("AC21")),  # AC21 = SD Card CS_n
+        IOStandard("LVCMOS33"),
+    ),
+    # unused acts as dummy pin, not routed to relevant location.
+    ("unused", 0, Pins("AC20"), IOStandard("LVCMOS33"))  # AC20 = SD Card MISO
+]
--- dax/gateware/targets/kc705.py
+++ dax/gateware/targets/kc705.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+import logging
+import itertools
+
 from artiq.gateware.targets.kc705 import *
 from artiq.gateware.targets.kc705 import _RTIOCRG, _StandaloneBase
+
+import dax.gateware.euriqa as euriqa
+
+# Logger
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.INFO)  # Set default logging level
 
 
 class KC705_BARE(_StandaloneBase):
@@ -82,24 +91,217 @@ class KC705_BARE(_StandaloneBase):
         self.csr_devices.append("rtio_analyzer")
 
 
-VARIANTS = {cls.__name__.lower(): cls for cls in [KC705_BARE, NIST_CLOCK, NIST_QC2, SMA_SPI]}
+class EURIQA(_StandaloneBase):
+    """EURIQA setup (red chamber)."""
+
+    def __init__(self, **kwargs):
+        """Declare hardware available on EURIQA's KC705 & Duke Breakout."""
+
+        add_sandia_dac_spi = kwargs.pop("sandia_dac_spi", False)
+        _StandaloneBase.__init__(self, **kwargs)
+        unused_count = itertools.count()
+
+        platform = self.platform
+        platform.add_extension(euriqa.fmc_adapter_io)
+        if add_sandia_dac_spi:
+            # segment to prevent accidentally adding x100 DAC comm/pins
+            platform.add_extension(euriqa.x100_dac_spi)
+
+        rtio_channels = list()
+
+        # USER_SMA_GPIO_P
+        phy = ttl_serdes_7series.InOut_8X(platform.request("user_sma_gpio_p_33"))
+        self.submodules += phy
+        rtio_channels.append(rtio.Channel.from_phy(phy, ififo_depth=512))
+
+        # USER_SMA_GPIO_N
+        phy = ttl_serdes_7series.InOut_8X(platform.request("user_sma_gpio_n_33"))
+        self.submodules += phy
+        rtio_channels.append(rtio.Channel.from_phy(phy, ififo_depth=512))
+
+        for i in range(2, 4):
+            # GPIO_LED_[2..3]
+            phy = ttl_simple.Output(platform.request("user_led", i))
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy))
+
+        ams101_dac = self.platform.request("ams101_dac", 0)
+        phy = ttl_simple.Output(ams101_dac.ldac)
+        self.submodules += phy
+        rtio_channels.append(rtio.Channel.from_phy(phy))
+
+        phy = spi2.SPIMaster(ams101_dac)
+        self.submodules += phy
+        rtio_channels.append(rtio.Channel.from_phy(
+            phy, ififo_depth=4))
+
+        phy = spi2.SPIMaster(platform.request("sdcard_spi_33"))
+        self.submodules += phy
+        rtio_channels.append(rtio.Channel.from_phy(
+            phy, ififo_depth=4))
+
+        self.config["HAS_RTIO_LOG"] = None
+        self.config["RTIO_LOG_CHANNEL"] = len(rtio_channels)
+        rtio_channels.append(rtio.LogChannel())
+
+        self.add_rtio(rtio_channels)
+
+        # Output GPIO/TTL Banks
+        for bank, i in itertools.product(["out1", "out2", "out3"], range(8)):
+            # out1-0, out1-1, ..., out3-7
+            if add_sandia_dac_spi and bank == "out2" and i == 7:
+                # add unused dummy channel. to keep channel #s same.
+                # Won't output to useful digital line
+                phy = ttl_serdes_7series.Output_8X(
+                    platform.request("unused", next(unused_count))
+                )
+            else:
+                phy = ttl_serdes_7series.Output_8X(platform.request(bank, i))
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy))
+        _LOGGER.info("Ending output GPIO chan: %i", len(rtio_channels))
+
+        # Input GPIO/TTL Banks
+        for bank, i in itertools.product(["in1", "in2", "in3", "in4", "in5"], range(8)):
+            # in1-0, in1-1, ..., in5-7
+            phy = ttl_serdes_7series.InOut_8X(platform.request(bank, i))
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy, ififo_depth=512))
+        _LOGGER.info("Ending input GPIO chan: %i", len(rtio_channels))
+
+        # User-controlled LED's? Not in device_db
+        for i in range(2, 4):
+            phy = ttl_simple.Output(platform.request("user_led", i))
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy))
+        _LOGGER.info("Ending output LED chan: %i", len(rtio_channels))
+
+        # Tri-state buffer to disable the TTL/GPIO outputs (out1, ...)
+        phy = ttl_simple.Output(platform.request("oeb", 0))
+        self.submodules += phy
+        rtio_channels.append(rtio.Channel.from_phy(phy))
+        _LOGGER.info("OEB GPIO chan: %i", len(rtio_channels))
+
+        # TODO: figure out usage/what this is
+        for i in range(9):
+            phy = ttl_serdes_7series.Output_8X(platform.request("sma", i))
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy))
+        _LOGGER.info("Ending sma chan: %i", len(rtio_channels))
+
+        # TODO: update name for io_update everywhere
+        # Update triggers for DDS. Edge will trigger output settings update
+        for i in range(8):
+            phy = ttl_serdes_7series.Output_8X(
+                platform.request("io_update", i))
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy))
+        _LOGGER.info("Ending io_update GPIO chan: %i", len(rtio_channels))
+
+        # Reset lines for the DDS boards.
+        for i in range(4):
+            phy = ttl_serdes_7series.Output_8X(platform.request("reset", i))
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy))
+        _LOGGER.info("Ending DDS Reset GPIO chan: %i", len(rtio_channels))
+
+        # SPI interfaces to control the DDS board outputs
+        for i in range(4):
+            spi_bus = self.platform.request("spi", i)
+            phy = spi2.SPIMaster(spi_bus)
+            self.submodules += phy
+            rtio_channels.append(rtio.Channel.from_phy(phy, ififo_depth=128))
+            odd_channel_sdio = platform.request("odd_channel_sdio", i)
+            self.comb += odd_channel_sdio.eq(spi_bus.mosi)
+        _LOGGER.info("Ending SPI chan: %i", len(rtio_channels))
+
+        # SPI & Load DAC (LDAC) pins for Controlling 8x DAC (DAC 8568)
+        spi_bus = self.platform.request("spi", 4)
+        phy = spi2.SPIMaster(spi_bus)
+        self.submodules += phy
+        rtio_channels.append(
+            rtio.Channel.from_phy(phy, ififo_depth=128)
+        )
+        _LOGGER.info("DAC8568 SPI RTIO channel: %i", len(rtio_channels))
+        phy = ttl_simple.Output(platform.request("ldac", 0))
+        self.submodules += phy
+        rtio_channels.append(rtio.Channel.from_phy(phy))
+        _LOGGER.info("DAC8568 LDAC GPIO channel: %i", len(rtio_channels))
+
+        # SPI for Core device serial comm to Sandia DAC
+        if add_sandia_dac_spi:
+            print("Adding SPI for Sandia DAC comms")
+            spi_bus = self.platform.request("spi", 5)
+            phy = spi2.SPIMaster(spi_bus)
+            self.submodules += phy
+            rtio_channels.append(
+                rtio.Channel.from_phy(phy, ififo_depth=128)
+            )
+
+        self.config["HAS_RTIO_LOG"] = None
+        self.config["RTIO_LOG_CHANNEL"] = len(rtio_channels)
+        _LOGGER.info("RTIO log chan: %i", len(rtio_channels))
+        rtio_channels.append(rtio.LogChannel())
+
+        _LOGGER.info("Euriqa KC705 RTIO channels: %s",
+                     list(enumerate(rtio_channels)))
+        self.add_rtio(rtio_channels)
+
+    def add_rtio(self, rtio_channels):
+        self.submodules.rtio_crg = _RTIOCRG(self.platform, self.crg.cd_sys.clk, use_sma=False)
+        self.csr_devices.append("rtio_crg")
+        self.config["HAS_RTIO_CLOCK_SWITCH"] = None
+        self.submodules.rtio_tsc = rtio.TSC("async", glbl_fine_ts_width=3)
+        self.submodules.rtio_core = rtio.Core(self.rtio_tsc, rtio_channels)
+        self.csr_devices.append("rtio_core")
+        self.submodules.rtio = rtio.KernelInitiator(self.rtio_tsc)
+        self.submodules.rtio_dma = ClockDomainsRenamer("sys_kernel")(
+            rtio.DMA(self.get_native_sdram_if()))
+        self.register_kernel_cpu_csrdevice("rtio")
+        self.register_kernel_cpu_csrdevice("rtio_dma")
+        self.submodules.cri_con = rtio.CRIInterconnectShared(
+            [self.rtio.cri, self.rtio_dma.cri],
+            [self.rtio_core.cri])
+        self.register_kernel_cpu_csrdevice("cri_con")
+        self.submodules.rtio_moninj = rtio.MonInj(rtio_channels)
+        self.csr_devices.append("rtio_moninj")
+
+        self.platform.add_period_constraint(self.rtio_crg.cd_rtio.clk, 8.)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.rtio_crg.cd_rtio.clk)
+
+        self.submodules.rtio_analyzer = rtio.Analyzer(self.rtio_tsc, self.rtio_core.cri,
+                                                      self.get_native_sdram_if())
+        self.csr_devices.append("rtio_analyzer")
+
+
+# Update the available variants
+VARIANTS.update({cls.__name__.lower(): cls for cls in [KC705_BARE, EURIQA]})
 
 
 def main():
+    # Parse arguments
     parser = argparse.ArgumentParser(description="KC705 gateware and firmware builder")
     builder_args(parser)
     soc_kc705_args(parser)
-    parser.set_defaults(output_dir="artiq_kc705")
-    parser.add_argument("-V", "--variant", choices=VARIANTS, default="kc705_bare")
+    parser.set_defaults(output_dir="kc705")
+    parser.add_argument("-V", "--variant", choices=VARIANTS)
+    parser.add_argument("-v", "--verbosity", action="count", default=0,
+                        help="increase logging verbosity level (default=WARNING)")
+    parser.add_argument("--sandia-dac-spi", action="store_true",
+                        help="Add SPI for real-time Sandia 100x DAC serial communication (EURIQA only)")
     args = parser.parse_args()
 
-    variant = args.variant.lower()
-    try:
-        cls = VARIANTS[variant]
-    except KeyError:
-        raise SystemExit("Invalid variant (-V/--variant)")
+    # TODO: verbosity argument might be unused at this moment
 
-    soc = cls(**soc_kc705_argdict(args))
+    # Prepare kwargs
+    kwargs = soc_kc705_argdict(args)
+    if cls is EURIQA:
+        kwargs.update(sandia_dac_spi=args.sandia_dac_spi)
+
+    # Build
+    soc = cls(**kwargs)
     build_artiq_soc(soc, builder_argdict(args))
 
 
