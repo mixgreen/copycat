b890243507299880f4d6d7aecd9602f42c90f253
Leon Riesebos <leon.riesebos@duke.edu>
2020-05-04 16:44:15 -0400

    sphinx path fix resulted in flake8 E402. added ignore annotation.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
-from dax import __version__ as dax_version
+from dax import __version__ as dax_version  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
