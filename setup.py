1b44416e7409385897ee1f843ca3b828669f4e5e
Brad Bondurant <brad.bondurant@duke.edu>
2020-05-07 17:45:38 -0400

    move package metadata from setup.py to setup.cfg (except info that comes from calls to versioneer)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import sys
 
 import versioneer
@@ -8,24 +8,7 @@ import versioneer
 if sys.version_info[:3] < (3, 5, 3):
     raise Exception("This software requires Python 3.5.3+")
 
-requirements = [
-    # "artiq",  # Needs to be installed manually (see ARTIQ installation instructions)
-    "numpy",
-    "scipy",
-    "pyvcd",
-    "natsort",
-    "pygit2",
-]
-
 setup(
-    name="dax",
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="Duke University",
-    author_email="",
-    description="Duke ARTIQ Extensions",
-    install_requires=requirements,
-    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
+    cmdclass=versioneer.get_cmdclass()
 )
