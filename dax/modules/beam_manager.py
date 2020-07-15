a84a11274bcd4b6a7878040271590c594fa23a83
Leon Riesebos <leon.riesebos@duke.edu>
2020-07-15 17:37:40 -0400

    finalized beam manager docstrings and NO_BEAM constant.

--- dax/modules/beam_manager.py
+++ dax/modules/beam_manager.py
@@ -3,19 +3,11 @@ import typing
 
 from dax.experiment import *
 
-__all__ = ['NO_BEAM', 'BeamAssignmentError', 'BeamManager']
-
-NO_BEAM: np.int32 = np.int32(-1)
-"""Constant value for no beam."""
-
-
-class BeamAssignmentError(RuntimeError):
-    """Exception type in case of errors during beam assignment."""
-    pass
+__all__ = ['BeamAssignmentError', 'BeamManager']
 
 
 class BeamManager(DaxModule):
-    """Module for managing a set of beams shared over a larger number of targets.
+    """Module for managing a set of beams shared over a number of targets.
 
     The beam manager contains a kernel-friendly reservation algorithm that is able to manage
     the state of a set of beams shared over a number of targets. The manager object only
@@ -26,12 +18,15 @@ class BeamManager(DaxModule):
     are shared over an ion chain using mirrors for targeting.
     """
 
+    NO_BEAM: np.int32 = np.int32(-1)
+    """Constant value for no beam."""
+
     class _BeamConfig:
         """Class that holds the configuration/state of a beam."""
 
         def __init__(self):
             # Initially we assume the beam is not targeted
-            self.target: np.int32 = NO_BEAM
+            self.target: np.int32 = BeamManager.NO_BEAM
             # We assume beams are initially off
             self.state: bool = False
 
@@ -46,6 +41,9 @@ class BeamManager(DaxModule):
         self._num_beams: np.int32 = np.int32(num_beams)
         self.logger.debug(f'Number of beams: {self._num_beams:d}')
         self.update_kernel_invariants('_num_beams')
+
+        # Mark class constant as kernel invariant
+        self.update_kernel_invariants('NO_BEAM')
 
         # Current beam configurations
         self._beam_config: typing.List[BeamManager._BeamConfig] = [self._BeamConfig() for _ in range(num_beams)]
@@ -67,13 +65,15 @@ class BeamManager(DaxModule):
         for acting on the results returned by the reservation system.
 
         When switching to the on state:
-         - Returns :attr:`NO_BEAM` if a beam was already on and targeted (no action required)
-         - Returns a beam index when a beam is available but not targeted
-         - Raises exception when no beam was targeted and no beams are available
+
+        - Returns :attr:`NO_BEAM` if a beam was already on and targeted (no action required)
+        - Returns a beam index when a beam is available but not targeted
+        - Raises exception when no beam was targeted and no beams are available
 
         When switching to the off state:
-         - Returns :attr:`NO_BEAM` if no beam is on targeted (no action required)
-         - Returns a beam index when a beam is on and targeted
+
+        - Returns :attr:`NO_BEAM` if no beam is on and targeted (no action required)
+        - Returns a beam index when a beam is on and targeted
 
         :param target: The desired beam target
         :param state: The desired state
@@ -83,7 +83,7 @@ class BeamManager(DaxModule):
 
         if state:  # We switch a beam on, which means we need to find a beam
             # Keep track of available beams in case we do not find a beam matching the target
-            available_beam = NO_BEAM
+            available_beam = self.NO_BEAM
 
             for b in range(self._num_beams):
                 # Search for a beam matching the target
@@ -92,7 +92,7 @@ class BeamManager(DaxModule):
                     # We found a beam matching the target
                     if c.state == state:
                         # Beam already targeted and in the correct state, nothing needs to be done
-                        return NO_BEAM
+                        return self.NO_BEAM
                     else:
                         # We found a beam matching the target with a different state, update state and return beam index
                         c.state = state
@@ -102,7 +102,7 @@ class BeamManager(DaxModule):
                     available_beam = b
 
             # We exhausted the search which means that no beam is targeted at the given target
-            if available_beam != NO_BEAM:
+            if available_beam != self.NO_BEAM:
                 # Save configuration
                 self._beam_config[available_beam].target = np.int32(target)  # Extra conversion for type safety
                 self._beam_config[available_beam].state = state
@@ -120,14 +120,14 @@ class BeamManager(DaxModule):
                     # We found a beam matching the target
                     if c.state == state:
                         # Beam already targeted and in the correct state, nothing needs to be done
-                        return NO_BEAM
+                        return self.NO_BEAM
                     else:
                         # We found a beam matching the target with a different state, update state and return beam index
                         c.state = state
                         return b
 
             # We exhausted the search which means that no beam is targeted at the given target
-            return NO_BEAM
+            return self.NO_BEAM
 
     @portable
     def set_beam(self, beam: TInt32, target: TInt32, state: TBool):
@@ -142,3 +142,8 @@ class BeamManager(DaxModule):
         """
         self._beam_config[beam].target = np.int32(target)  # Extra conversion for type safety
         self._beam_config[beam].state = state
+
+
+class BeamAssignmentError(RuntimeError):
+    """Exception type in case of errors during beam assignment."""
+    pass
--- test/modules/beam_manager.py
+++ test/modules/beam_manager.py
@@ -1,7 +1,7 @@
 import unittest
 
 from dax.experiment import *
-from dax.modules.beam_manager import BeamManager, NO_BEAM, BeamAssignmentError
+from dax.modules.beam_manager import BeamManager, BeamAssignmentError
 from dax.util.artiq import get_manager_or_parent
 
 
@@ -23,6 +23,7 @@ class GetBeam2TestCase(unittest.TestCase):
     def setUp(self) -> None:
         assert self.N > 0, 'Number of beams N should be > 0'
         self.s = _TestSystem(get_manager_or_parent(), num_beams=self.N)
+        self.bm: BeamManager = self.s.m
 
     def test_num_beams_bounds(self):
         # Keep track of returned indices
@@ -30,19 +31,19 @@ class GetBeam2TestCase(unittest.TestCase):
 
         # Allocate all beams
         for t in range(self.N):
-            b = self.s.m.get_beam(t, True)
-            self.assertNotEqual(b, NO_BEAM, 'Returned NO_BEAM while action was required')
+            b = self.bm.get_beam(t, True)
+            self.assertNotEqual(b, self.bm.NO_BEAM, 'Returned NO_BEAM while action was required')
             self.assertNotIn(b, beam_index, 'A beam index was returned twice')
             beam_index.add(b)  # Add beam index to keep track
 
         # Allocate with same targets, does not violate bounds
         for t in range(self.N):
-            b = self.s.m.get_beam(t, True)
-            self.assertEqual(b, NO_BEAM, 'Did not return NO_BEAM while no action was required')
+            b = self.bm.get_beam(t, True)
+            self.assertEqual(b, self.bm.NO_BEAM, 'Did not return NO_BEAM while no action was required')
 
         # Violate bounds by allocating one more beam
         with self.assertRaises(BeamAssignmentError, msg='Number of beams bound violation did not raise'):
-            self.s.m.get_beam(self.N, True)
+            self.bm.get_beam(self.N, True)
 
     def test_free_beam(self):
         for _ in range(self.N * 2):
@@ -51,14 +52,14 @@ class GetBeam2TestCase(unittest.TestCase):
 
             # Allocate all beams
             for t in range(self.N):
-                b = self.s.m.get_beam(t, True)
+                b = self.bm.get_beam(t, True)
                 self.assertNotIn(b, beam_dict, 'A beam index was returned twice')
                 beam_dict[b] = t  # Add beam index with target to keep track
 
             # Free all beams
             for t in range(self.N):
-                b = self.s.m.get_beam(t, False)
-                self.assertNotEqual(b, NO_BEAM, 'Returned NO_BEAM while action was required')
+                b = self.bm.get_beam(t, False)
+                self.assertNotEqual(b, self.bm.NO_BEAM, 'Returned NO_BEAM while action was required')
                 self.assertIn(b, beam_dict, 'Returned a non-assigned beam index')
                 # Remove beam index to keep track and verify if beam index corresponds to assigned one
                 self.assertEqual(t, beam_dict.pop(b), 'Returned beam index does not match earlier assigned index')
@@ -66,8 +67,8 @@ class GetBeam2TestCase(unittest.TestCase):
     def test_free_unassigned_beam(self):
         for t in range(self.N * 4):  # Test values in and outside the range of N
             # Free a beam that was never assigned
-            b = self.s.m.get_beam(t, False)
-            self.assertEqual(b, NO_BEAM, 'Did not return NO_BEAM while no action was required')
+            b = self.bm.get_beam(t, False)
+            self.assertEqual(b, self.bm.NO_BEAM, 'Did not return NO_BEAM while no action was required')
 
     def test_reassigned_beam(self):
         for i in range(self.N):
@@ -77,41 +78,41 @@ class GetBeam2TestCase(unittest.TestCase):
 
                 # Allocate all beams
                 for t in range(self.N):
-                    b = self.s.m.get_beam(t, True)
+                    b = self.bm.get_beam(t, True)
                     self.assertNotIn(b, beam_dict, 'A beam index was returned twice')
                     beam_dict[b] = t  # Add beam index with target to keep track
 
                 # Free one beam
-                b = self.s.m.get_beam(i, False)
-                self.assertNotEqual(b, NO_BEAM, 'Returned NO_BEAM while action was required')
+                b = self.bm.get_beam(i, False)
+                self.assertNotEqual(b, self.bm.NO_BEAM, 'Returned NO_BEAM while action was required')
                 self.assertIn(b, beam_dict, 'Returned a non-assigned beam index')
                 # Remove beam index to keep track and verify if beam index corresponds to assigned one
                 self.assertEqual(i, beam_dict.pop(b), 'Returned beam index does not match earlier assigned index')
                 old_b = b
 
                 # Reassign earlier freed beam
-                b = self.s.m.get_beam(i, True)
+                b = self.bm.get_beam(i, True)
                 self.assertNotIn(b, beam_dict, 'A beam index was returned twice')
                 self.assertEqual(old_b, b, 'Target was not reassigned to earlier beam')
                 beam_dict[b] = i  # Add beam index with target to keep track
 
                 # Free all beams
                 for t in range(self.N):
-                    b = self.s.m.get_beam(t, False)
-                    self.assertNotEqual(b, NO_BEAM, 'Returned NO_BEAM while action was required')
+                    b = self.bm.get_beam(t, False)
+                    self.assertNotEqual(b, self.bm.NO_BEAM, 'Returned NO_BEAM while action was required')
                     self.assertIn(b, beam_dict, 'Returned a non-assigned beam index')
                     # Remove beam index to keep track and verify if beam index corresponds to assigned one
                     self.assertEqual(t, beam_dict.pop(b), 'Returned beam index does not match earlier assigned index')
 
                 # Reassign earlier freed beam one more time
-                b = self.s.m.get_beam(i, True)
+                b = self.bm.get_beam(i, True)
                 self.assertNotIn(b, beam_dict, 'A beam index was returned twice')
                 self.assertEqual(old_b, b, 'Target was not reassigned to earlier beam')
                 beam_dict[b] = i  # Add beam index with target to keep track
 
                 # Free one beam one more time
-                b = self.s.m.get_beam(i, False)
-                self.assertNotEqual(b, NO_BEAM, 'Returned NO_BEAM while action was required')
+                b = self.bm.get_beam(i, False)
+                self.assertNotEqual(b, self.bm.NO_BEAM, 'Returned NO_BEAM while action was required')
                 self.assertIn(b, beam_dict, 'Returned a non-assigned beam index')
                 # Remove beam index to keep track and verify if beam index corresponds to assigned one
                 self.assertEqual(i, beam_dict.pop(b), 'Returned beam index does not match earlier assigned index')
