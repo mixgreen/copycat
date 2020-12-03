7a26a8fe2531bf53c632ad78d0262fd1aaf048a5
Leon Riesebos <leon.riesebos@duke.edu>
2020-12-03 12:10:25 -0500

    updated dax.sim time and edge counter test cases

---
+++ test/sim/coredevice/test_edge_counter.py
@@ -0,0 +1,104 @@
+import unittest
+import random
+
+from artiq.experiment import *
+
+import dax.sim.test_case
+
+from test.environment import CI_ENABLED
+
+_NUM_SAMPLES = 1000 if CI_ENABLED else 100
+
+_INPUT_FREQ = 200e3
+
+_DEVICE_DB = {
+    'core': {
+        'type': 'local',
+        'module': 'artiq.coredevice.core',
+        'class': 'Core',
+        'arguments': {'host': '0.0.0.0', 'ref_period': 1e-9}
+    },
+    'ec': {
+        "type": "local",
+        "module": "artiq.coredevice.edge_counter",
+        "class": "EdgeCounter",
+        "arguments": {},
+        "sim_args": {"input_freq": _INPUT_FREQ},
+    }
+}
+
+
+class _Environment(HasEnvironment):
+    def build(self):
+        self.core = self.get_device('core')
+        self.ec = self.get_device('ec')
+
+
+class EdgeCounterTestCase(dax.sim.test_case.PeekTestCase):
+
+    def setUp(self) -> None:
+        self.env = self.construct_env(_Environment, device_db=_DEVICE_DB)
+
+    def test_gate_rising_mu(self):
+        for _ in range(_NUM_SAMPLES):
+            duration = random.randrange(1000000, 1000000000)
+            ref_time = now_mu()
+            self.env.ec.gate_rising_mu(duration)
+
+            self.assertEqual(now_mu(), ref_time + duration)
+            self.assertEqual(self.env.ec.fetch_count(), int(self.env.core.mu_to_seconds(duration) * _INPUT_FREQ))
+
+    def test_gate_falling_mu(self):
+        for _ in range(_NUM_SAMPLES):
+            duration = random.randrange(1000000, 1000000000)
+            ref_time = now_mu()
+            self.env.ec.gate_falling_mu(duration)
+
+            self.assertEqual(now_mu(), ref_time + duration)
+            self.assertEqual(self.env.ec.fetch_count(), int(self.env.core.mu_to_seconds(duration) * _INPUT_FREQ))
+
+    def test_gate_both_mu(self):
+        for _ in range(_NUM_SAMPLES):
+            duration = random.randrange(1000000, 1000000000)
+            ref_time = now_mu()
+            self.env.ec.gate_both_mu(duration)
+
+            self.assertEqual(now_mu(), ref_time + duration)
+            self.assertEqual(self.env.ec.fetch_count(), int(self.env.core.mu_to_seconds(duration) * _INPUT_FREQ * 2))
+
+    def test_set_config_gate_rising(self):
+        for _ in range(_NUM_SAMPLES):
+            duration = random.randrange(1000000, 1000000000)
+            ref_time = now_mu()
+            self.env.ec.set_config(True, False, False, True)  # Start gate
+            delay_mu(duration)
+            self.env.ec.set_config(False, False, True, False)  # Stop gate
+
+            self.assertEqual(now_mu(), ref_time + duration)
+            self.assertEqual(self.env.ec.fetch_count(), int(self.env.core.mu_to_seconds(duration) * _INPUT_FREQ))
+
+    def test_set_config_gate_falling(self):
+        for _ in range(_NUM_SAMPLES):
+            duration = random.randrange(1000000, 1000000000)
+            ref_time = now_mu()
+            self.env.ec.set_config(False, True, False, True)  # Start gate
+            delay_mu(duration)
+            self.env.ec.set_config(False, False, True, False)  # Stop gate
+
+            self.assertEqual(now_mu(), ref_time + duration)
+            self.assertEqual(self.env.ec.fetch_count(), int(self.env.core.mu_to_seconds(duration) * _INPUT_FREQ))
+
+    def test_set_config_gate_both(self):
+        for _ in range(_NUM_SAMPLES):
+            duration = random.randrange(1000000, 1000000000)
+            ref_time = now_mu()
+            self.env.ec.set_config(True, True, False, True)  # Start gate
+            delay_mu(duration)
+            self.env.ec.set_config(False, False, True, False)  # Stop gate
+
+            self.assertEqual(now_mu(), ref_time + duration)
+            self.assertEqual(self.env.ec.fetch_count(), int(self.env.core.mu_to_seconds(duration) * _INPUT_FREQ * 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/sim/test_time.py
+++ test/sim/test_time.py
@@ -73,16 +73,142 @@ class TimeManagerTestCase(unittest.TestCase):
                 # Compare to reference time
                 self.assertEqual(now_mu(), ref_time, 'Reference does not match now_mu()')
 
-    def test_parallel(self):
-        # Reference time
-        ref_time = now_mu()
+    def test_shallow_parallel(self):
+        # Shallow parallel matches the timing model of ARTIQ, see https://github.com/m-labs/artiq/issues/1555
+
+        range_stop = 1000000000
 
         for i in range(_NUM_SAMPLES):
             with self.subTest('parallel sub test', i=i):
+                # Reference time
+                ref_time = now_mu()
+
                 # Block duration
                 block_duration = 0
                 with parallel:
-                    for _ in range(10):
+                    # Random duration
+                    duration = self.rnd.randrange(range_stop)
+                    # Delay
+                    delay_mu(duration)
+                    # Update block duration
+                    block_duration = max(duration, block_duration)
+
+                    # Manually repeat test code to keep them top-level statements in the shallow parallel context
+                    duration = self.rnd.randrange(range_stop)
+                    delay_mu(duration)
+                    block_duration = max(duration, block_duration)
+                    duration = self.rnd.randrange(range_stop)
+                    delay_mu(duration)
+                    block_duration = max(duration, block_duration)
+                    duration = self.rnd.randrange(range_stop)
+                    delay_mu(duration)
+                    block_duration = max(duration, block_duration)
+
+                # Update ref time
+                ref_time += block_duration
+                # Compare to reference time
+                self.assertEqual(now_mu(), ref_time, 'Reference does not match now_mu()')
+
+    def test_explicit_shallow_parallel(self):
+        # Shallow parallel matches the timing model of ARTIQ, see https://github.com/m-labs/artiq/issues/1555
+
+        for i in range(_NUM_SAMPLES):
+            with self.subTest('parallel sub test', i=i):
+                # Reference time
+                ref_time = now_mu()
+
+                # Block duration
+                block_duration = 0
+                # Single random duration
+                duration = self.rnd.randrange(1000000000)
+
+                with parallel:
+                    # Delay
+                    delay_mu(duration)
+                    # Update block duration
+                    block_duration = max(duration, block_duration)  # Top-level statement, so parallel
+
+                    if True:
+                        # We are not a top level statement anymore, so timing is sequential
+                        with sequential:  # Add an explicit sequential context!
+                            sequential_duration = 0
+                            for _ in range(10):
+                                delay_mu(duration)
+                                sequential_duration += duration
+
+                            # Parallelize the sequential block with other parallel top-level statements
+                            block_duration = max(sequential_duration, block_duration)
+
+                # Update ref time
+                ref_time += block_duration
+                # Compare to reference time
+                self.assertEqual(now_mu(), ref_time, 'Reference does not match now_mu()')
+
+    def test_manual_deep_parallel(self):
+        # Manual deep parallel matches the timing model of ARTIQ, see https://github.com/m-labs/artiq/issues/1555
+
+        for i in range(_NUM_SAMPLES):
+            with self.subTest('parallel sub test', i=i):
+                # Reference time
+                ref_time = now_mu()
+
+                # Block duration
+                block_duration = 0
+                # Single random duration
+                duration = self.rnd.randrange(1000000000)
+
+                for _ in range(10):
+                    at_mu(ref_time)  # Manually revert timeline for deep parallel emulation
+                    # Delay
+                    delay_mu(duration)
+                    # Update block duration
+                    block_duration = max(duration, block_duration)
+
+                # Update ref time
+                ref_time += block_duration
+                # Compare to reference time
+                self.assertEqual(now_mu(), ref_time, 'Reference does not match now_mu()')
+
+    @unittest.expectedFailure
+    def test_not_deep_parallel(self):
+        # Deep parallel does not match the timing model of ARTIQ, see https://github.com/m-labs/artiq/issues/1555
+        # But DAX.sim implements deep parallel! The parallel semantics of ARTIQ and DAX.sim differ here!
+
+        for i in range(_NUM_SAMPLES):
+            with self.subTest('parallel sub test', i=i):
+                # Reference time
+                ref_time = now_mu()
+
+                # Block duration
+                block_duration = 0
+                with parallel:
+                    for _ in range(10):  # Only the for-loop is a top-level statement, not the content inside the loop!
+                        # with sequential:  # The implicit sequential context will be added here
+                        # Random duration
+                        duration = self.rnd.randrange(1000000000)
+                        # Delay
+                        delay_mu(duration)
+                        # Update block duration
+                        block_duration += duration  # Add duration to block duration, implicit sequential
+
+                # Update ref time
+                ref_time += block_duration
+                # Compare to reference time
+                self.assertEqual(now_mu(), ref_time, 'Reference does not match now_mu()')
+
+    def test_deep_parallel(self):
+        # Deep parallel does not match the timing model of ARTIQ, see https://github.com/m-labs/artiq/issues/1555
+        # But DAX.sim implements deep parallel! The parallel semantics of ARTIQ and DAX.sim differ here!
+
+        for i in range(_NUM_SAMPLES):
+            with self.subTest('parallel sub test', i=i):
+                # Reference time
+                ref_time = now_mu()
+
+                # Block duration
+                block_duration = 0
+                with parallel:
+                    for _ in range(10):  # Deep parallel propagates beyond top-level statements
                         # Random duration
                         duration = self.rnd.randrange(1000000000)
                         # Delay
@@ -131,10 +257,11 @@ class WatchdogTestCase(unittest.TestCase):
 
     def test_watchdog_in_time(self):
         with watchdog(self.WINDOW):
-            with parallel:
-                for _ in range(_NUM_SAMPLES):
-                    # All delays are less than the window, so no exceptions should be raised
-                    delay(self.WINDOW * self.rnd.random())
+            t = now_mu()
+            for _ in range(_NUM_SAMPLES):
+                # All delays are less than the window, so no exceptions should be raised
+                at_mu(t)
+                delay(self.WINDOW * self.rnd.random())
 
         for _ in range(_NUM_SAMPLES):
             with watchdog(self.WINDOW):
@@ -147,13 +274,12 @@ class WatchdogTestCase(unittest.TestCase):
 
     def test_watchdog_expired(self):
         with watchdog(self.WINDOW):
-            with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in nested parallel context did not raise'):
-                with parallel:
-                    # More than a window, so exception should be raised
-                    delay(self.WINDOW * (self.rnd.random() + 2))
+            with self.assertRaises(WatchdogExpired, msg='Watchdog timeout did not raise'):
+                # More than a window, so exception should be raised
+                delay(self.WINDOW * (self.rnd.random() + 2))
 
         with watchdog(self.WINDOW):
-            with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in nested sequential context did not raise'):
+            with self.assertRaises(WatchdogExpired, msg='Watchdog timeout did not raise'):
                 # More than a window, so exception should be raised
                 delay(self.WINDOW + self.REF_PERIOD)
 
@@ -161,10 +287,11 @@ class WatchdogTestCase(unittest.TestCase):
         with watchdog(self.WINDOW):
             with watchdog(self.WINDOW):
                 with watchdog(self.WINDOW):
-                    with parallel:
-                        for _ in range(_NUM_SAMPLES):
-                            # All delays are less than the window, so no exceptions should be raised
-                            delay(self.WINDOW * self.rnd.random())
+                    t = now_mu()
+                    for _ in range(_NUM_SAMPLES):
+                        # All delays are less than the window, so no exceptions should be raised
+                        at_mu(t)
+                        delay(self.WINDOW * self.rnd.random())
 
         for _ in range(_NUM_SAMPLES):
             with watchdog(self.WINDOW):
@@ -183,29 +310,27 @@ class WatchdogTestCase(unittest.TestCase):
         with watchdog(self.WINDOW):
             with watchdog(self.WINDOW):
                 with watchdog(self.WINDOW):
-                    with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in nested parallel context '
+                    with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in inner nested watchdog context '
                                                                 'did not raise'):
-                        with parallel:
-                            # More than a window, so exception should be raised
-                            delay(self.WINDOW * (self.rnd.random() + 2))
+                        # More than a window, so exception should be raised
+                        delay(self.WINDOW * (self.rnd.random() + 2))
 
-        with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in nested parallel context did not raise'):
+        with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in outer nested watchdog context did not raise'):
             with watchdog(self.WINDOW):
                 with watchdog(self.WINDOW):
                     with watchdog(self.WINDOW):
-                        with parallel:
-                            # More than a window, so exception should be raised
-                            delay(self.WINDOW * (self.rnd.random() + 2))
+                        # More than a window, so exception should be raised
+                        delay(self.WINDOW * (self.rnd.random() + 2))
 
         with watchdog(self.WINDOW):
             with watchdog(self.WINDOW):
                 with watchdog(self.WINDOW):
-                    with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in nested sequential context '
+                    with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in inner nested watchdog context '
                                                                 'did not raise'):
                         # More than a window, so exception should be raised
                         delay(self.WINDOW + self.REF_PERIOD)
 
-        with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in nested sequential context did not raise'):
+        with self.assertRaises(WatchdogExpired, msg='Watchdog timeout in outer nested watchdog context did not raise'):
             with watchdog(self.WINDOW):
                 with watchdog(self.WINDOW):
                     with watchdog(self.WINDOW):
