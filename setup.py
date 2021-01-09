30bb61394ee16c435e96b17d6b0b6ba0f9cabfd3
Leon Riesebos <leon.riesebos@duke.edu>
2021-01-09 14:23:18 -0500

    updated setup.cfg with python_requires

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
-import sys
-
 import versioneer
-
-if sys.version_info[:2] < (3, 7):
-    raise Exception("This software requires Python 3.7+")
 
 setup(
     version=versioneer.get_version(),
