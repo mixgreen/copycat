b6e9396c15ad24917dc8d435b4d08bc3b5bfdd73
Leon Riesebos <leon.riesebos@duke.edu>
2021-03-06 19:01:34 -0500

    re-added versioneer override feature

--- versioneer.py
+++ versioneer.py
@@ -1,4 +1,3 @@
-
 # Version: 0.19
 
 """The Versioneer - like a rocketeer, but for versions.
@@ -341,6 +340,7 @@ def get_config_from_root(root):
         if parser.has_option("versioneer", name):
             return parser.get("versioneer", name)
         return None
+
     cfg = VersioneerConfig()
     cfg.VCS = VCS
     cfg.style = get(parser, "style") or ""
@@ -365,12 +365,14 @@ HANDLERS = {}
 
 def register_vcs_handler(vcs, method):  # decorator
     """Create decorator to mark a method as the handler of a VCS."""
+
     def decorate(f):
         """Store f in HANDLERS[vcs][method]."""
         if vcs not in HANDLERS:
             HANDLERS[vcs] = {}
         HANDLERS[vcs][method] = f
         return f
+
     return decorate
 
 
@@ -897,6 +899,10 @@ def get_versions():
     # py2exe/bbfreeze/non-CPython implementations don't do __file__, in which
     # case we can only use expanded keywords.
 
+    override = os.getenv("VERSIONEER_OVERRIDE")
+    if override:
+        return {"version": override, "full-revisionid": None, "dirty": None, "error": None, "date": None}
+
     cfg = get_config()
     verbose = cfg.verbose
 
@@ -1407,6 +1413,10 @@ def get_versions(verbose=False):
 
     Returns dict with two keys: 'version' and 'full'.
     """
+    override = os.getenv("VERSIONEER_OVERRIDE")
+    if override:
+        return {"version": override, "full-revisionid": None, "dirty": None, "error": None, "date": None}
+
     if "versioneer" in sys.modules:
         # see the discussion in cmdclass.py:get_cmdclass()
         del sys.modules["versioneer"]
@@ -1528,6 +1538,7 @@ def get_cmdclass(cmdclass=None):
             print(" date: %s" % vers.get("date"))
             if vers["error"]:
                 print(" error: %s" % vers["error"])
+
     cmds["version"] = cmd_version
 
     # we override "build_py" in both distutils and setuptools
@@ -1566,6 +1577,7 @@ def get_cmdclass(cmdclass=None):
                                                   cfg.versionfile_build)
                 print("UPDATING %s" % target_versionfile)
                 write_to_version_file(target_versionfile, versions)
+
     cmds["build_py"] = cmd_build_py
 
     if "setuptools" in sys.modules:
@@ -1591,6 +1603,7 @@ def get_cmdclass(cmdclass=None):
                                               cfg.versionfile_source)
             print("UPDATING %s" % target_versionfile)
             write_to_version_file(target_versionfile, versions)
+
     cmds["build_ext"] = cmd_build_ext
 
     if "cx_Freeze" in sys.modules:  # cx_freeze enabled?
@@ -1622,6 +1635,7 @@ def get_cmdclass(cmdclass=None):
                              "PARENTDIR_PREFIX": cfg.parentdir_prefix,
                              "VERSIONFILE_SOURCE": cfg.versionfile_source,
                              })
+
         cmds["build_exe"] = cmd_build_exe
         del cmds["build_py"]
 
@@ -1648,6 +1662,7 @@ def get_cmdclass(cmdclass=None):
                              "PARENTDIR_PREFIX": cfg.parentdir_prefix,
                              "VERSIONFILE_SOURCE": cfg.versionfile_source,
                              })
+
         cmds["py2exe"] = cmd_py2exe
 
     # we override different "sdist" commands for both environments
@@ -1678,6 +1693,7 @@ def get_cmdclass(cmdclass=None):
             print("UPDATING %s" % target_versionfile)
             write_to_version_file(target_versionfile,
                                   self._versioneer_generated_versions)
+
     cmds["sdist"] = cmd_sdist
 
     return cmds
