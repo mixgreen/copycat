6bf9d3b2210ed8a3a17d92e8e9d05ead8146449b
Grant Costa <grcosta95@gmail.com>
2022-09-21 16:36:47 -0400

    Added config dir parsing of single line config files

--- dax/modules/trap_dc.py
+++ dax/modules/trap_dc.py
@@ -1,4 +1,5 @@
 from __future__ import annotations  # Postponed evaluation of annotations
+import dataclasses
 from functools import lru_cache
 
 import math
@@ -21,7 +22,132 @@ _ZOTINO_SOLUTION_T = typing.List[_ZOTINO_LINE_T]
 _ZOTINO_LINE_T_MU = typing.Tuple[_ZOTINO_KEY_T_MU, _ZOTINO_VALUE_T]
 _ZOTINO_SOLUTION_T_MU = typing.List[_ZOTINO_LINE_T_MU]
 
-__all__ = ['TrapDcModule', 'ZotinoReader']
+__all__ = ['TrapDcModule', 'ZotinoReader', 'DacConfig']
+
+
+@dataclasses.dataclass(frozen=True)
+class _DacConfigAttrs:
+    min: float
+    max: float
+    unit: str
+    ndecimals: int = 4
+    scale: typing.Optional[float] = 1.0
+    step: typing.Optional[float] = None
+
+    def in_range(self, value) -> bool:
+        assert isinstance(value, float)
+        assert self.min < self.max
+        return self.min <= value <= self.max
+
+
+_D_UNIT: float = 2.74
+"""Natural distance unit (in um) corresponding to MHz^2 units for the quadrupoles (see OneNote)."""
+
+# TODO: Should change this to a file perhaps?
+_DAC_CONFIG_ATTRS: typing.Dict[str, _DacConfigAttrs] = {
+    'dx': _DacConfigAttrs(-100.0, +100.0, unit='V/m', ndecimals=3),
+    'dy': _DacConfigAttrs(-100.0, +100.0, unit='V/m', ndecimals=3),
+    'dz': _DacConfigAttrs(-2e3, +2e3, unit='V/m', ndecimals=3),
+    'x1': _DacConfigAttrs(-0.2, +0.2, unit=f'{_D_UNIT} um * MHz^2', ndecimals=4),
+    'x2': _DacConfigAttrs(-6.0, +6.0, unit='MHz^2', ndecimals=4),
+    'x3': _DacConfigAttrs(-0.014, +0.014, unit=f'MHz^2 / ({_D_UNIT} um)', ndecimals=4, step=1e-4),
+    'x4': _DacConfigAttrs(-6e-3, +6e-3, unit=f'MHz^2 / ({_D_UNIT} um)^2', ndecimals=5, step=1e-3),
+    'qxz': _DacConfigAttrs(-6.0, +6.0, unit='MHz^2', ndecimals=4),
+    'qzz': _DacConfigAttrs(-6.0, +6.0, unit='MHz^2', ndecimals=4),
+    'qzy': _DacConfigAttrs(-6.0, +6.0, unit='MHz^2', ndecimals=4),
+    'center': _DacConfigAttrs(-50.0, +50.0, unit='MHz^2', ndecimals=2),
+}
+"""Attributes for each DAC config field."""
+
+
+@dataclasses.dataclass
+class DacConfig:
+    dx: float
+    dy: float
+    dz: float
+    x1: float
+    x2: float
+    x3: float
+    x4: float
+    qxz: float
+    qzz: float
+    qzy: float
+    center: float
+
+    def as_dict(self) -> typing.Dict[str, typing.Any]:
+        return dataclasses.asdict(self)
+
+    def verify(self) -> None:
+        for f, v in self.as_dict().items():
+            attrs = _DAC_CONFIG_ATTRS[f]
+            if not attrs.in_range(v):
+                raise ValueError(f'Field {f}={v} is out of range [{attrs.min}, {attrs.max}]')
+
+    def to_dataset_sys(self, module: TrapDcModule, group: str, **kwargs: typing.Any) -> None:
+        assert isinstance(module, TrapDcModule)
+        assert isinstance(group, str)
+        assert group.isalpha()
+
+        for f, v in self.as_dict().items():
+            module.set_dataset_sys(f'{group}.{f}', v, **kwargs)
+
+    def __add__(self, other: DacConfig) -> DacConfig:
+        assert isinstance(other, DacConfig)
+
+        fields = self.as_dict()
+        for f, v in dataclasses.asdict(other).items():
+            fields[f] += v
+        return DacConfig(**fields)
+
+    @classmethod
+    def from_module(cls, module: TrapDcModule, group: str, **kwargs: typing.Any) -> DacConfig:
+        assert isinstance(module, TrapDcModule)
+        assert isinstance(group, str)
+        assert group.isalpha()
+        assert isinstance(kwargs.get('default', 0.0), float)
+        assert isinstance(kwargs.get('fallback', 0.0), float)
+
+        fields = {f: module.get_dataset_sys(f'{group}.{f}', **kwargs) for f in cls.fields()}
+        return DacConfig(**fields)
+
+    @classmethod
+    def from_arguments(cls, env: HasEnvironment, defaults: DacConfig, *,
+                       prefix: str = '', group: typing.Optional[str] = None) -> DacConfig:
+        assert isinstance(env, HasEnvironment)
+        assert isinstance(defaults, DacConfig)
+
+        fields = {
+            f: cls.get_argument(env, f, default=getattr(defaults, f), prefix=prefix, group=group)
+            for f in cls.fields()
+        }
+        return DacConfig(**fields)
+
+    @classmethod
+    def get_argument(cls, env: HasEnvironment, field: str, default: float, *,
+                     prefix: str = '', group: typing.Optional[str], tooltip: typing.Optional[str] = None,
+                     **kwargs: typing.Any):
+        assert isinstance(env, HasEnvironment)
+        assert isinstance(field, str)
+        assert isinstance(default, float)
+        assert isinstance(prefix, str)
+        assert isinstance(group, str) or group is None
+        assert isinstance(tooltip, str) or tooltip is None
+
+        if group is None:
+            group = 'Sandia DAC offset overrides'
+
+        number_kwargs = dataclasses.asdict(_DAC_CONFIG_ATTRS[field])
+        number_kwargs.update(kwargs)
+
+        return env.get_argument(
+            f'{prefix}{field.upper()}',
+            NumberValue(default, **number_kwargs),
+            group=group, tooltip=tooltip
+        )
+
+    @classmethod
+    def fields(cls) -> typing.Sequence[str]:
+        return [f.name for f in dataclasses.fields(cls)]
 
 
 class TrapDcModule(DaxModule):
@@ -47,18 +173,26 @@ class TrapDcModule(DaxModule):
     _DMA_STARTUP_TIME: typing.ClassVar[float] = 1.728 * us
     """Startup time for DMA (s). Measured in the RTIO benchmarking tests during CI"""
 
+    GLOBAL_GROUP_KEY = 'global'
+    """Dataset key for dac config global group"""
+    OFFSET_GROUP_KEY = 'offset'
+    """Dataset key for dac config offset group"""
+
     _zotino: artiq.coredevice.zotino.Zotino
     _solution_path: pathlib.Path
     _map_file: pathlib.Path
     _reader: ZotinoReader
     _min_line_delay_mu: np.int64
     _calculator: ZotinoCalculator
+    _adjustment_lines: typing.Dict[str, _ZOTINO_LINE_T]
+    _adjustment_gains: typing.Dict[str, float]
 
     def build(self,  # type: ignore[override]
               *,
               key: str,
               solution_path: str,
-              map_file: str) -> None:
+              map_file: str,
+              config_path: typing.Optional[str] = None) -> None:
         """Build the trap DC module
 
         :param key: The key of the zotino device
@@ -68,6 +202,7 @@ class TrapDcModule(DaxModule):
         assert isinstance(key, str)
         assert isinstance(solution_path, str)
         assert isinstance(map_file, str)
+        assert not config_path or isinstance(config_path, str)
 
         # Get devices
         self._zotino = self.get_device(key, artiq.coredevice.zotino.Zotino)
@@ -83,8 +218,27 @@ class TrapDcModule(DaxModule):
         self._reader = ZotinoReader(
             self._solution_path, self._map_file)
 
+        self._args_enabled = False
+
+        # names of the possible adjustments
+        if config_path:
+            self._adjustment_lines = self._get_config_lines(config_path)
+
+        self._adjustment_gains: typing.Dict[str, float] = {}
+
     @host_only
-    def init(self) -> None:
+    def _get_config_lines(self, config_path: str) -> typing.Dict[str, _ZOTINO_LINE_T]:
+        adjustments = {}
+        for k in _DAC_CONFIG_ATTRS:
+            file_name = config_path + '/' + k + '.csv'
+            try:
+                adjustments[k] = self._reader.process_solution(self._reader.read_solution(file_name))[0]
+            except FileNotFoundError:
+                self.logger.warning(f"Couldn't find config file for {k} in {config_path} directory")
+        return adjustments
+
+    @host_only
+    def init(self, *, reset: bool = False) -> None:
         """Initialize this module."""
         # Get profile loader
         # Below calculated from set_dac_mu and load functions
@@ -217,6 +371,10 @@ class TrapDcModule(DaxModule):
                      for i, t in enumerate(trimmed_solution[1:])])
 
         return path
+
+    @host_only
+    def get_config_masks(self, dir: pathlib.Path, vars: typing.Union[typing.Sequence[str], str]):
+        return None
 
     @host_only
     def list_solutions(self) -> typing.Sequence[str]:
@@ -480,6 +638,131 @@ class TrapDcModule(DaxModule):
                                    comm_delay_slope_mu=comm_delay_slope_mu,
                                    dma_comm_delay_intercept_mu=dma_comm_delay_intercept_mu,
                                    dma_comm_delay_slope_mu=dma_comm_delay_slope_mu)
+
+    @host_only
+    def update_global(self, global_: DacConfig) -> None:
+        """Update global configuration in system datasets.
+
+        :param global_: The new global configuration
+        """
+        global_.to_dataset_sys(self, self.GLOBAL_GROUP_KEY)
+
+    @host_only
+    def update_offset(self, offset: DacConfig) -> None:
+        """Update offset configuration in system datasets.
+
+        :param offset: The new offset configuration
+        """
+        offset.to_dataset_sys(self, self.OFFSET_GROUP_KEY)
+
+    def _add_arguments(self, env: HasEnvironment, *,
+                       global_: bool, offset: bool,
+                       enable: typing.Optional[bool], group: typing.Optional[str]) -> None:
+        assert isinstance(env, HasEnvironment)
+        assert isinstance(global_, bool)
+        assert isinstance(offset, bool)
+        assert isinstance(enable, bool) or enable is None
+        assert isinstance(group, str) or group is None
+
+        try:
+            self._args_enabled
+        except AttributeError:
+            raise RuntimeError('System build function was not called before adding DAC arguments') from None
+
+        # Enable argument
+        self.logger.debug(f'Adding configuration arguments: global={global_}, offset={offset}')
+        if enable is None:
+            self._args_enabled = True
+        else:
+            self._args_enabled = env.get_argument(
+                'Override DAC configuration', BooleanValue(enable), group=group,
+                tooltip='Override DAC configuration with the values provided by the arguments below'
+            )
+
+        # Config arguments
+        if global_:
+            self._args_global_cfg = DacConfig.from_arguments(
+                env, DacConfig.from_module(self, self.GLOBAL_GROUP_KEY, fallback=0.0),
+                prefix='Global ', group=group
+            )
+        if offset:
+            self._args_offset_cfg = DacConfig.from_arguments(
+                env, DacConfig.from_module(self, self.OFFSET_GROUP_KEY, fallback=0.0),
+                prefix='Offset ', group=group
+            )
+
+    @host_only
+    def add_global_arguments(self, env: HasEnvironment, *,
+                             enable: typing.Optional[bool] = False,
+                             group: typing.Optional[str] = 'DAC configuration') -> None:
+        """Add arguments to the experiment for overriding global configuration only (using system offset configuration).
+
+        This function can only be called during the build phase.
+        This function must be called **after** any system build function calls.
+
+        :param env: The ARTIQ environment object that is in the build phase (normally ``self``)
+        :param enable: Enable usage of arguments by default (use :const:`None` to force usage of arguments)
+        :param group: Argument group name (optional)
+        """
+        self._add_arguments(env, enable=enable, group=group,
+                            global_=True, offset=False)
+
+    @host_only
+    def add_offset_arguments(self, env: HasEnvironment, *,
+                             enable: typing.Optional[bool] = False,
+                             group: typing.Optional[str] = 'DAC configuration') -> None:
+        """Add arguments to the experiment for overriding offset configuration only (using system offset configuration).
+
+        This function can only be called during the build phase.
+        This function must be called **after** any system build function calls.
+
+        :param env: The ARTIQ environment object that is in the build phase (normally ``self``)
+        :param enable: Enable usage of arguments by default (use :const:`None` to force usage of arguments)
+        :param group: Argument group name (optional)
+        """
+        self._add_arguments(env, enable=enable, group=group,
+                            global_=False, offset=True)
+
+    @rpc
+    def config(self, global_, offset):  # type: (DacConfig, DacConfig) -> None
+        """Set global and offset configuration and update compensations.
+
+        :param global_: The new global configuration
+        :param offset: The new offset configuration
+        """
+        assert isinstance(global_, DacConfig)
+        assert isinstance(offset, DacConfig)
+
+        # Verify and add configurations
+        self.logger.debug(f'Global: {global_}')
+        self.logger.debug(f'Offset: {offset}')
+        global_.verify()
+        offset.verify()
+        cfg = global_ + offset
+        cfg.verify()
+
+        # Update compensations
+        self._adjustment_gains["dx"] = cfg.dx / 1e3
+        self._adjustment_gains["dy"] = cfg.dy / 1e3
+        self._adjustment_gains["dz"] = cfg.dz / 1e3
+
+        self._adjustment_gains["x1"] = (
+            cfg.x1
+            - cfg.x2 * (cfg.center / _D_UNIT)
+            + cfg.x3 * (cfg.center / _D_UNIT) ** 2 / 2.0
+            - cfg.x4 * (cfg.center / _D_UNIT) ** 3 / 6.0
+        )
+        self._adjustment_gains["x2"] = (
+            cfg.x2
+            - cfg.x3 * (cfg.center / _D_UNIT)
+            + cfg.x4 * (cfg.center / _D_UNIT) ** 2 / 2.0
+        )
+        self._adjustment_gains["x3"] = cfg.x3 - (cfg.x4 * (cfg.center / _D_UNIT))
+        self._adjustment_gains["x4"] = cfg.x4
+
+        self._adjustment_gains["qxz"] = cfg.qxz
+        self._adjustment_gains["qzz"] = cfg.qzz
+        self._adjustment_gains["qzy"] = cfg.qzy
 
 
 class ZotinoCalculator:
